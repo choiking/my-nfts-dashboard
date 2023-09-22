febf6d25d249543fd61a948f6b6a22a8bd0f661e
choiking <samsamuels1927@gmail.com>
2023-09-22 17:18:48 +0800

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,9 +3,7 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img
-					src="https://uploads-ssl.webflow.com/63d4bbe756bafb75b9894c92/63f01a1963ea22041414804b_Frame%20427318983.svg"
-				></img>
+				Logo Image
 			</a>
 		</nav>
 	);
